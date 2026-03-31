e1796dc53665537ab42fb810e4ea62bc1fee325b
dilmikavishka <dilmikavishka14@gmail.com>
2026-03-31 16:42:01 +0530

    update code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   apps : [
     {
+      name: "cloud-sql-auth-proxy",
+      script: "./cloud-sql-proxy project-c7ceb7ed-6723-4a8a-8f7:asia-southeast1:mysql-vm project-c7ceb7ed-6723-4a8a-8f7:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy.log",
+    },
+
+    {
       name: "book-service",
       script: "java",
       args: "-jar books-service/target/books-service-0.0.1-SNAPSHOT.jar",
